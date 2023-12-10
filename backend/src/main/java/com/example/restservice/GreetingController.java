f585cad6c7ab2b8cf28b2491bca7c77fa349bfe1
tnk <tnk72@web.de>
2023-12-10 11:04:33 +0100

    first commit

---
+++ backend/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,29 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PostMapping("greetingPost")
+	public void greetingPost(@RequestBody String body){
+		logger.debug("post", body);
+	}
+
+}
